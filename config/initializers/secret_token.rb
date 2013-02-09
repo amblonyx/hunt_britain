87a9be663731f6105ffa9d182c4c5558ac710e6b
Maureen Hadert <amblonyx@gmail.com>
2013-02-09 10:56:39 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HuntBritain::Application.config.secret_token = 'f580bb805a61389add435848c4bd1a166af8578cec26d7d66ce424c498bcafff79a1a7bd87802b547d498e0eb38b109caa18d18a0093bd1d06b541630576d85a'
