433ce65f67628790db0604fd6c66cf66a040f159
OtterAdrift <amblonyx@gmail.com>
2013-03-10 13:59:57 +0000

    Hunt process now working

--- app/helpers/hunts_helper.rb
+++ app/helpers/hunts_helper.rb
@@ -1,13 +1,19 @@
 module HuntsHelper
 
-	def five_answers(xml)	
-		a = xml.xpath("//outcome/selection/*")
+	def five_answers(xml, correct)	
+		# Put the answers (except the correct one) in an array and get four random ones
+		a = xml.xpath("//outcome/selection/option[@id!='" + correct + "']")
 		aarray = []
 		a.each do |node|
 			aarray.push(node)
 		end
 		aarray.shuffle!
-		afive = aarray[0..4]
+		afive = aarray[0..3]
+		# Add the correct answer to the four, and shuffle again
+		correctnode = xml.xpath("//outcome/selection/option[@id='" + correct + "']").first
+		afive.push(correctnode)
+		afive.shuffle!
+		# Put the five answers into a hash to return to the view
 		n = 0
 		afive.each do |node|
 			ahash = {}
