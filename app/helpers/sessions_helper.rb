1106ae48dd9988abe33edbe03d36a7905d111016
OtterAdrift <amblonyx@gmail.com>
2013-03-17 21:06:56 +0000

    Refactoring to get a common approach

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,64 +1,56 @@
 module SessionsHelper
 
+	# let and set...  @current_user is local variable
+	def current_user=(user)
+		@current_user = user
+	end
+	
+	def current_user
+		# if current_user is NOT nil, set it using remember_token
+		@current_user  ||= User.find_by_remember_token(cookies[:remember_token])
+	end
+	
 	def sign_in(user)
 		cookies.permanent[:remember_token] = user.remember_token
 		self.current_user = user
 	end
-	def remember_guest(email)
-		cookies[:guest] = email
-		cookies[:remember_token] = "guest"
-	end
-	def identified?
-		!current_user.nil? || !session[:guest].blank?
-	end
-	def signed_in?
-		!current_user.nil? 
-	end
-	def guest_user
-		user = User.new
-		user.create_as_guest session[:guest]
-		session.delete(:guest)	# so their email isn't stored if they leave the page
-		user
-	end
+	
 	def correct_user?(user)
 		user == current_user
 	end
+
 	def sign_out
 		self.current_user = nil
 		cookies.delete(:remember_token)
 		cookies.delete(:guest)
 	end
-	def redirect_back_or(default)
-		redirect_to(session[:return_to] || default)
-		session.delete(:return_to)
-	end
-	def store_location
-		session[:return_to] = request.fullpath
-	end
-	def has_address?
-		current_user.address_1.blank? or current_user.postcode.blank?
+
+	def identified?
+		!current_user.nil? || !session[:guest].blank?
 	end
 	
-	def not_signed_in_user(params)
-		unless signed_in?
-			store_location
-			redirect_to signin_path(params)#, notice: "Please sign in."
-		end
+	def signed_in?
+		!current_user.nil? 
 	end
-	def user_has_no_address(params)
-		unless has_address?
-			store_location
-			redirect_to edit_user_path(params)#, notice: "Please enter an address" 
-		end
-	end
+#	def make_guest_user
+#		user = User.new
+#		user.create_as_guest session[:guest]
+#		session.delete(:guest)	# so their email isn't stored if they leave the page
+#		user
+#	end
 	
-	# let and set...  @current_user is local variable
-	def current_user=(user)
-		@current_user = user
-	end
-	def current_user
-		# if current_user is NOT nil, set it using remember_token
-		@current_user  ||= User.find_by_remember_token(cookies[:remember_token])
-	end
+#	def not_signed_in_user #(params)
+#		unless signed_in?
+#			store_location
+#			redirect_to signin_path #(params)#, notice: "Please sign in."
+#		end
+#	end
+	
+#	def user_has_no_address(params)
+#		unless current_user.have_address?
+#			store_location
+#			redirect_to edit_user_path(params)#, notice: "Please enter an address" 
+#		end
+#	end
 	
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,4 @@
 module UsersHelper
-	def has_purchase_format(cart, *formats)
-		cart.each_with_index do |cart_item, index|
-			product = Product.find(cart_item[:product_id])
-			if !product.nil?
-				if !formats.index(product.format).nil?
-					return true 
-				end
-			end
-		end
-		false
-	end
 	def output_address(user)
 		if !user.nil?
 			address = output_address_line(user.address_1)
