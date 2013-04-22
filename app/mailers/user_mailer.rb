64adb33e5fdb4fc33d7f26728845906fe192f769
Maureen Hadert <amblonyx@gmail.com>
2013-04-22 21:49:15 +0100

    Tidy up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,16 @@
 class UserMailer < ActionMailer::Base
-  default from: "huntbritain@gmail.com"
+	default from: "huntbritain@gmail.com"
+
+	def welcome_email(user)
+		@user = user
+		@url  = signin_path 
+		mail(to: user.email, subject: 'Welcome to Hunt Britain')
+	end
   
-    def welcome_email(user)
-    @user = user
-    @url  = signin_path 
-    mail(to: user.email, subject: 'Welcome to Hunt Britain')
-  end
+	def feedback_email(name, email, message)
+		@name = name
+		@email = email
+		@message = message
+		mail(to: "treasurehuntbritain@gmail.com", subject: 'Feedback from Treasure Hunt Britain')		
+	end
 end
