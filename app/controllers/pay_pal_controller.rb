01a6036d074c55178656bcab832ae461a7a994d1
Maureen Hadert <amblonyx@gmail.com>
2013-05-05 13:55:27 +0100

    paypal tch

--- app/controllers/pay_pal_controller.rb
+++ app/controllers/pay_pal_controller.rb
@@ -7,12 +7,15 @@ class PayPalController < ApplicationController
 	end
   
 	def notify
+		#for testing:
+		#http://localhost:3000/paypal_notify?mc_gross=19.95&protection_eligibility=Eligible&address_status=confirmed&payer_id=LPLWNMTBWMFAY&tax=0.00&address_street=1+Main+St&payment_date=20%3A12%3A59+Jan+13%2C+2009+PST&payment_status=Completed&charset=windows-1252&address_zip=95131&first_name=Test&mc_fee=0.88&address_country_code=US&address_name=Test+User&notify_version=2.6&custom=&payer_status=verified&address_country=United+States&address_city=San+Jose&quantity=1&verify_sign=AtkOfCXbDm2hu0ZELryHFjY-Vb7PAUvS6nMXgysbElEn9v-1XcmSoGtf&payer_email=gpmac_1231902590_per%40paypal.com&txn_id=61E67681CH3238416&payment_type=instant&last_name=User&address_state=CA&receiver_email=gpmac_1231902686_biz%40paypal.com&payment_fee=0.88&receiver_id=S8XGHLYDW9T3S&txn_type=express_checkout&item_name=&mc_currency=USD&item_number=&residence_country=US&test_ipn=1&handling_amount=0.00&transaction_subject=&payment_gross=19.95&shipping=0.0
+		
 		#handle PAYPAL notification
 		notify = Paypal::Notification.new request.raw_post
 
 		# TODO: find the purchase in the DB?		
 
-		if notify.acknowledge
+		#if notify.acknowledge
 			notification = IpnLog.new
 			notification.purchase_id = notify.item_id
 			notification.item_id = notify.item_id
@@ -21,9 +24,12 @@ class PayPalController < ApplicationController
 			notification.fee = notify.fee
 			notification.gross = notify.gross
 			notification.invoice = notify.invoice
-			notification.received_at = DateTime.now
+		#	notification.received_at = notify.received_at
 			notification.status = notify.status
 			notification.type = notify.type
+			
+			if notify.complete? then notification.complete = true end
+			if notify.test? then notification.test = true end
 			
 			if notification.save 
 			
@@ -37,11 +43,12 @@ class PayPalController < ApplicationController
 				# todo update purchase
 			end 
 			
-			render :nothing => true
+		#	render :nothing => true
+		
+		#else
+			#LOG hacking attempt?
 			
-		#rescue
-			#LOG Exceptions
-		end
+		#end
 		
 	end
   
