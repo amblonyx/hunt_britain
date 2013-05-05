105ba7579b9557aa73d9f41dab1c327b5e53c764
Maureen Hadert <amblonyx@gmail.com>
2013-05-05 22:03:30 +0100

    More paypal

--- app/controllers/pay_pal_controller.rb
+++ app/controllers/pay_pal_controller.rb
@@ -7,9 +7,8 @@ class PayPalController < ApplicationController
 	end
   
 	def notify
-		#for testing:
-		#http://localhost:3000/paypal_notify?mc_gross=19.95&protection_eligibility=Eligible&address_status=confirmed&payer_id=LPLWNMTBWMFAY&tax=0.00&address_street=1+Main+St&payment_date=20%3A12%3A59+Jan+13%2C+2009+PST&payment_status=Completed&charset=windows-1252&address_zip=95131&first_name=Test&mc_fee=0.88&address_country_code=US&address_name=Test+User&notify_version=2.6&custom=&payer_status=verified&address_country=United+States&address_city=San+Jose&quantity=1&verify_sign=AtkOfCXbDm2hu0ZELryHFjY-Vb7PAUvS6nMXgysbElEn9v-1XcmSoGtf&payer_email=gpmac_1231902590_per%40paypal.com&txn_id=61E67681CH3238416&payment_type=instant&last_name=User&address_state=CA&receiver_email=gpmac_1231902686_biz%40paypal.com&payment_fee=0.88&receiver_id=S8XGHLYDW9T3S&txn_type=express_checkout&item_name=&mc_currency=USD&item_number=&residence_country=US&test_ipn=1&handling_amount=0.00&transaction_subject=&payment_gross=19.95&shipping=0.0
-		
+		#Resource on IPN: https://cms.paypal.com/uk/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_admin_IPNIntro
+			
 		#handle PAYPAL notification
 		notify = Paypal::Notification.new request.raw_post
 
@@ -21,20 +20,29 @@ class PayPalController < ApplicationController
 		notification.fee = notify.fee
 		notification.gross = notify.gross
 		notification.invoice = notify.invoice
-		notification.received_at = notify.received_at
+		notification.received_at = DateTime.now #notify.received_at
 		notification.status = notify.status
 		notification.type = notify.type
+		notification.ipn_string = request.query_string
 		
-		if notify.complete? then notification.complete = true end
-		if notify.test? then notification.test = true end
+		notification.complete = notify.complete?
+		notification.test = notify.test?
 		
 		if notify.acknowledge
-			# Find the purchase and set it to PAID	
+			# Find the purchase 	
 			@purchase = Purchase.find_by_id(notify.item_id)
 			
-			if notify.complete? and @purchase.total_price == notify.gross 
+			# Perform additional checks to make sure that notify is legitimate and not already processed
+			if notify.complete? and @purchase.total_price == notify.gross and @purchase.id == notify.item_id and @purchase.reference == notify.invoice and @purchase.status != "Paid"
 				@purchase.status = "Paid"
 				@purchase.save
+
+				# Send a confirmation email for purchase
+				UserMailer.confirm_purchase(@purchase).deliver
+				# Deliver online and PDF hunts
+				UserMailer.deliver_purchases(@purchase).deliver
+			else
+				notification.status = "ANOMALY"
 			end 
 			
 		else 
@@ -42,7 +50,7 @@ class PayPalController < ApplicationController
 		end
 
 		notification.save 
-		render :nothing => true
+		render nothing: true
 		
 	end
   
